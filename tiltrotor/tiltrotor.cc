9f2295dcd5010873edc2b7819525ce97ccf5640b
Lenny Khazan <lenny.khazan@gmail.com>
2017-10-16 20:53:47 -0400

    Added states

--- tiltrotor/tiltrotor.cc
+++ tiltrotor/tiltrotor.cc
@@ -1,6 +1,28 @@
 #include "Arduino.h"
 #include "controller.h"
 
+void setup();
+void loop();
+void loop_hover();
+void loop_transition_flying();
+void loop_fly();
+void loop_transition_hovering();
+
+typedef enum {
+  // In hover mode
+  STATE_HOVER = 0,
+
+  // Transitioning to flying mode
+  STATE_TRANSITION_FLYING,
+
+  // In flying mode
+  STATE_FLY,
+
+  // Transitioning to hover mode
+  STATE_TRANSIITON_HOVERING
+} STATE;
+
+STATE state = STATE_HOVER;
 Controller controller(0, 0, 0, 0);
 
 void setup() {
@@ -8,5 +30,37 @@ void setup() {
 }
 
 void loop() {
-  InputState s = controller.get_state();
+  switch (state) {
+    case STATE_HOVER:
+    loop_hover();
+    break;
+
+    case STATE_TRANSITION_FLYING:
+    loop_transition_flying();
+    break;
+
+    case STATE_FLY:
+    loop_fly();
+    break;
+
+    case STATE_TRANSIITON_HOVERING:
+    loop_transition_hovering();
+    break;
+  }
+}
+
+void loop_hover() {
+
+}
+
+void loop_transition_flying() {
+
+}
+
+void loop_fly() {
+
+}
+
+void loop_transition_hovering() {
+
 }
